1d7c84267d205bb02f68707fb519ecaefe6dfc5f
kengthong <wookengthong@gmail.com>
2018-10-30 17:08:20 +0800

    added fashe

---
+++ Entry Insertion 51-100.sql
@@ -0,0 +1,161 @@
+--Entry Insertion
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Nalgene Bottle', 'Woodlands', 10 , 20, 5, 2, 1, '2018-10-20', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Nike Bottle', 'Kent Ridge', 5 , 12, 8, 2, 1, '2018-11-20', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Adidas Bottle', 'Sembawang', 2 , 12, 9, 2, 1, '2018-10-21', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Thumbdrive', 'Clementi', 5 , 55, 14, 2, 1, '2018-12-15', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Nike Football', 'Jurong', 10 , 45, 11, 1, 1, '2018-10-13', 21)
+
+--5 to 10
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Sudio niva', 'Changi', 50 , 65, 16, 2, 1, '2018-11-19', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Nike Watch', 'Pioneer', 52 , 62, 17, 1, 1, '2018-11-12', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('LV bag', 'Orchard', 82 , 92, 3, 1, 1, '2018-10-8', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Oakley Radar Sunglasses', 'Holland V.', 99 , 155, 7, 1, 1, '2018-12-11', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('American Football', 'Paya Lebar', 12 , 14, 6, 3, 2, '2018-10-15', 21)
+
+--11 to 15
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Logitech G2 Wheel', 'Ang Mo Kio', 55 , 65, 9, 1, 1, '2018-10-24', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Window Wiper', 'Bishan', 5 , 6, 1, 10, 1, '2018-11-9', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('iSucc Vacuum', 'Geylang', 2 , 12, 9, 2, 1, '2018-12-13', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Viper Thumbdrive', 'Geylang', 5 , 55, 14, 2, 1, '2018-12-15', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Nike Football boots', 'Windsor Road', 10 , 45, 11, 1, 1, '2018-10-13', 21)
+
+--16 to 20
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Ultimate Ears 2', 'Pasir Ris', 50 , 65, 16, 2, 1, '2018-11-19', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Field Glasses', 'Ubin', 52 , 62, 17, 1, 1, '2018-11-12', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Vans Shoe', 'Somerset', 82 , 92, 3, 3, 1, '2018-10-23', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Oakley Crosslink Sunglasses', 'Newton', 99 , 155, 7, 1, 1, '2018-12-11', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Lacrosse Stick', 'Harbourfront', 12 , 14, 14, 3, 2, '2018-10-15', 21)
+
+--21 to 25
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Lenovo Laptop', 'Ang Mo Kio', 200 , 365, 9, 1, 1, '2018-10-24', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Bandana', 'Bishan', 5 , 6, 1, 10, 1, '2018-11-9', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('iphone 2', 'Geylang', 2 , 12, 9, 2, 1, '2018-12-13', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Knight and Day Movie', 'Geylang', 5 , 55, 14, 2, 1, '2018-12-15', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Nike Football shin guard', 'Windsor Road', 10 , 45, 11, 1, 1, '2018-10-13', 21)
+
+--26 to 30
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Beats Headphone 2', 'Pasir Ris', 50 , 65, 16, 2, 1, '2018-11-19', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Field watch Seiko', 'Ubin', 52 , 62, 17, 1, 1, '2018-11-12', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Rubber boots', 'Somerset', 82 , 92, 3, 3, 1, '2018-10-23', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Rayban Sunglasses', 'Newton', 99 , 155, 7, 1, 1, '2018-12-11', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Floorball Stick', 'Harbourfront', 12 , 14, 14, 3, 2, '2018-10-15', 21)
+
+
+--31 to 35
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Cane', 'Toa Payoh', 200 , 365, 9, 1, 1, '2018-10-24', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Banana costume', 'Bishan', 5 , 6, 1, 10, 1, '2018-11-9', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('iphone X', 'Geylang', 2 , 12, 9, 2, 1, '2018-12-13', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Samsung Galaxy S9', 'Geylang', 55 , 155, 14, 2, 1, '2018-12-15', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Airpods', 'Upper Thomson', 10 , 45, 11, 1, 1, '2018-10-13', 21)
+
+--36 to 40
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Beats Speaker', 'Pasir Ris', 50 , 65, 16, 2, 1, '2018-11-19', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Field Jacket', 'Ubin', 52 , 62, 17, 1, 1, '2018-11-12', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Rubber boots', 'Somerset', 82 , 92, 3, 3, 1, '2018-10-23', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('LV Sunglasses', 'Newton', 99 , 155, 7, 1, 1, '2018-12-11', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Floorball ball', 'Harbourfront', 12 , 14, 14, 3, 2, '2018-10-15', 21)
+
+
+--41 to 45
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Camelbak Bottle', 'Woodlands', 10 , 20, 15, 2, 1, '2018-10-20', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Nike Cap', 'Kent Ridge', 5 , 12, 18, 2, 1, '2018-11-20', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Adidas NMD Ltd Ed', 'Sembawang', 112 , 115, 9, 2, 1, '2018-10-21', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Seagate Thumbdrive', 'Clementi', 5 , 55, 14, 2, 1, '2018-12-15', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('EPL Football', 'Jurong', 10 , 45, 11, 1, 1, '2018-10-13', 21)
+
+--45 to 50
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Jaybird X3', 'Changi', 50 , 65, 16, 2, 1, '2018-11-19', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Rolex Watch', 'Pioneer', 252 , 362, 17, 1, 1, '2018-11-12', 21)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('LV bag', 'Orchard', 82 , 92, 3, 1, 1, '2018-10-8', 7)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('Gucci HandBag', 'Holland V.', 99 , 155, 7, 1, 1, '2018-12-11', 14)
+
+INSERT INTO entry(name, location, starting_bid, current_bid, owner_id, total_quantity, current_quantity, bid_closing_date, loan_duration)
+VALUES ('American Tourister luggage', 'Paya Lebar', 122 , 144, 6, 3, 2, '2018-10-15', 21)
---
+++ sql files/User Insertion.sql
@@ -0,0 +1,63 @@
+--User insertion
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Deborah Ruiz', 'druiz0@drupal.org', 'password1', '89840801', 'Deborah84', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Tammy Lee', 'tlee1@barnesandnoble.com', 'password1', '89980914', 'Tammy1998', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Rebecca Garza', 'rgarza2@cornell.edu', 'password1', '99840611', 'RebeccaG84', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Walter Leong', 'wleong3@shop-pro.jp', 'password1', '99830626', 'Walter83', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin)  
+VALUES ('Kathryn Edwards', 'kedwards4@twitter.com', 'password1', '89931127', 'Kathryn1993', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Jonathan Bailey', 'jbailey5@webs.com', 'password1', '90001113', 'Jonathan2000', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Helen Hanson', 'hhanson6@mediafire.com', 'password1', '69980112', 'Helen1998', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Kathy Reid', 'kreid7@cbsnews.com', 'password1', '99830106', 'KathyR83', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Paula Armstrong', 'parmstrong8@furl.net', 'password1', '90001206', 'Paula2000', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Denise Gonzalez', 'dgonzalez9@slate.com', 'password1', '89821206', 'Denise82', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Matthew Vasquez', 'mvasqueza@nymag.com', 'password1', '89881031', 'MatthewV88', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Stephen Sullivan', 'ssullivanb@noaa.gov', 'password1', '89860306', 'StephenS86', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Carolyn Patterson', 'cpattersonc@liveinternet.ru', 'password1', '89990403', 'Carolyn1999', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Brenda Jenkins', 'bjenkinsd@mashable.com', 'password1', '89920916', 'Brenda1992', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Kathleen Kanh', 'kkanhe@walmart.com', 'password1', '80000107', 'Kathleen2000', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Ralph Thomas', 'rthomasf@imgur.com', 'password1', '69830606', 'RalphT83', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Todd Barnes', 'tbarnesg@sina.com.cn', 'password1', '69940719', 'Todd1994', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Roger Morrison', 'rmorrisonh@miitbeian.gov.cn', 'password1', '99950516', 'Roger1995', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Eric Jordan', 'ejordani@spiegel.de', 'password1', '69840531', 'EricJ84', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Denise Hayes', 'dhayesj@amazonaws.com', 'password1', '99890806', 'Denise89', FALSE);
+
+INSERT INTO users (name, email, password, contact_number, username, admin) 
+VALUES ('Douglas George', 'dgeorgek@behance.net', 'password1', '89960925', 'Douglas1996', FALSE);

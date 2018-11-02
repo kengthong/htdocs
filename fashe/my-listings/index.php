8fa89d0de2728739f9798efb57737b377acc6af1
y1bs <39340177+y1bs@users.noreply.github.com>
2018-11-02 16:18:44 +0800

    Test

--- fashe/my-listings/index.php
+++ fashe/my-listings/index.php
@@ -61,6 +61,7 @@
 
 					<div class="topbar-language rs1-select2">
 						<select class="selection-1" name="time">
+							<option>SGD</option>
 							<option>USD</option>
 							<option>EUR</option>
 						</select>
@@ -131,7 +132,16 @@
 
     <!-- PUT YOUR CODE HERE -->
     <div>
-        My listings. Functions required => CRUD, accept bid, reject bid,
+		My listings. Functions required => CRUD, accept bid, reject bid,
+		
+		<!-- Create Listing -->
+
+		<!-- Remove Listing -->
+
+		<!-- Update Listing -->
+		
+		<!-- Delete Listing -->
+
     </div>
 
 
@@ -145,7 +155,7 @@
 
 				<div>
 					<p class="s-text7 w-size27">
-						Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
+						Any questions? Let us know in store at National University of Singapore COM2 or call us on (+65) 9676 6879
 					</p>
 
 					<div class="flex-m p-t-30">
@@ -359,6 +369,37 @@
 
 <!--===============================================================================================-->
 	<script src="../js/main.js"></script>
+
+<?php
+  	// Connect to the database. Please change the password in the following line accordingly
+	$db     = pg_connect("host=localhost port=5432 dbname=Project1 user=postgres password=test");
+	//$db     = pg_connect("host=127.0.0.1  port=8080 dbname=cs2102Project user=postgres password=kengthong");
+    $result = pg_query($db, "SELECT * FROM book where book_id = '$_POST[bookid]'");		// Query template
+    $row    = pg_fetch_assoc($result);		// To store the result row
+    if (isset($_POST['submit'])) {
+        echo "<ul><form name='update' action='index.php' method='POST' >  
+    	<li>Book ID:</li>  
+    	<li><input type='text' name='bookid_updated' value='$row[book_id]' /></li>  
+    	<li>Book Name:</li>  
+    	<li><input type='text' name='book_name_updated' value='$row[name]' /></li>  
+    	<li>Price (USD):</li><li><input type='text' name='price_updated' value='$row[price]' /></li>  
+    	<li>Date of publication:</li>  
+    	<li><input type='text' name='dop_updated' value='$row[date_of_publication]' /></li>  
+    	<li><input type='submit' name='new' /></li>  
+    	</form>  
+    	</ul>";
+    }
+    if (isset($_POST['new'])) {	// Submit the update SQL command
+        $result = pg_query($db, "UPDATE book SET book_id = '$_POST[bookid_updated]',  
+    name = '$_POST[book_name_updated]',price = '$_POST[price_updated]',  
+    date_of_publication = '$_POST[dop_updated]'");
+        if (!$result) {
+            echo "Update failed!!";
+        } else {
+            echo "Update successful!";
+        }
+    }
+    ?>  
 
 </body>
 </html>
